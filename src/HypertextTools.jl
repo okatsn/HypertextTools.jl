8bb8f5fc1264529d7287dc6e3c033a677713a177
okatsn <okatsn@gmail.com>
2022-10-20 15:25:58 +0800

    Temporarily remove latextable
    It is now developed under swc-forecast-insider and it will be moved back some day.

--- src/HypertextTools.jl
+++ src/HypertextTools.jl
@@ -6,10 +6,9 @@ module HypertextTools
     include("weavehugo.jl")
 	export lazyhugo, cp2content, getdoc, defolder
 
-    using InteractiveUtils: clipboard # this is the old df2latex.jl
     using DataFrames
-    include("latextable.jl")
-    export latextable
+    # include("latextable.jl")
+    # export latextable
 	
     
 	include("renamefile.jl");
